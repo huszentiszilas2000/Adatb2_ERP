eed03b860dbf10579e84b3d5fb5b8be64ef59a53
szentiszilas <huszentiszilas@gmail.com>
2023-01-04 21:07:19 +0100

    Adatb2_ERP/erp_manager: Delete user history tab
    [SQL]
    
    Signed-off-by: szentiszilas <huszentiszilas@gmail.com>

--- erp_manager/tables/usertab_h.sql
+++
@@ -1,7 +0,0 @@
-create table usertab_h(
-       db_id            number,
-       azonosito        varchar2(20),
-       nev              varchar2(30),
-       aktiv            varchar(1),
-       adminisztrator   varchar(1) 
-);
