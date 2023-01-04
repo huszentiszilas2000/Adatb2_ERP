1fbc680307b9a36f2c4695c4dd47da032f4980df
szentiszilas <huszentiszilas@gmail.com>
2023-01-04 21:10:17 +0100

    Adatb2_ERP/erp_manager: Remove userta, alter, trg and rework soon
    [SQL]
    
    Signed-off-by: szentiszilas <huszentiszilas@gmail.com>

--- erp_manager/alter/usertab_alter.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE usertab
-      ADD CONSTRAINT aktiv_boolean CHECK (aktiv = 'Y' OR aktiv = 'N');
-      
-ALTER TABLE usertab
-      ADD CONSTRAINT adminisztrator_boolean CHECK (adminisztrator = 'Y' OR adminisztrator = 'N');
--- erp_manager/tables/usertab.sql
+++
@@ -1,7 +0,0 @@
-create table usertab(
-       db_id            number primary key,
-       azonosito        varchar2(20) not null,
-       nev              varchar2(30),
-       aktiv            varchar(1),
-       adminisztrator   varchar(1)   
-);
