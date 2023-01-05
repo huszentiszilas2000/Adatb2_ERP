b99c2ada3c0697010a75bb05aa97f43a69829a76
szentiszilas <huszentiszilas@gmail.com>
2023-01-05 19:37:49 +0100

    Adatb2_ERP/erp_manager: Modify install_all.sql
    [SQL]
    
    Signed-off-by: szentiszilas <huszentiszilas@gmail.com>

--- erp_manager/install_all.sql
+++ erp_manager/install_all.sql
@@ -1,1 +1,13 @@
-teszt
+PROMPT Installing ERP DataBase...
+
+PROMPT Installing Sequences...
+
+@./sequence/material_seq.sql
+@./sequence/partner_seq.sql
+@./sequence/salesinvoice_seq.sql
+@./sequence/salesinvoiceitem_seq.sql
+@./sequence/salesorder_seq.sql
+@./sequence/salesorderitem_seq.sql
+@./sequence/stock_seq.sql
+
+PROMPT Installing Sequences Done...
